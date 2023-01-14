3f758f0a1848eb710b63972923be3885531f8b87
Gabriel Braga <gabriel.bragasilva@hotmail.com>
2023-01-13 21:38:05 -0300

    Controller

---
+++ conversor_moedas/lib/app/controllers/home_controller.dart
@@ -0,0 +1,44 @@
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'package:flutter/cupertino.dart';
+
+import '../models/currency_model.dart';
+
+class HomeController {
+  List<CurrencyModel> currencies;
+
+  TextEditingController toText = TextEditingController();
+  TextEditingController fromText = TextEditingController();
+  
+
+  CurrencyModel toCurrency;
+  CurrencyModel fromCurrency;
+
+  HomeController(
+    this.currencies,
+    this.toCurrency,
+    this.fromCurrency,
+  ) {
+    currencies = CurrencyModel.getCurrencies();
+    toCurrency = currencies[0];
+    fromCurrency = currencies[1];
+  }
+
+  void converter(){
+
+    String text = toText.text;
+    double value = double.tryParse(text) ?? 1.0;
+    double returnValue = 0.0;
+
+    if(fromCurrency.name == 'Real'){
+      returnValue = value * toCurrency.real;
+    }else if(fromCurrency.name == 'Dolar'){
+      returnValue = value * toCurrency.dolar;
+    }else if(fromCurrency.name == 'Euro'){
+      returnValue = value * toCurrency.euro;
+    }else if(fromCurrency.name == 'Bitcoin'){
+      returnValue = value * toCurrency.bitcoin;
+    }
+
+    fromText.text = returnValue.toStringAsFixed(2);
+  }
+}
